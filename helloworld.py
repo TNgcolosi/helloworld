d025a645ed79f26ed5495144228471a06366ff50
Thandeka Bhengu <mazibani@gmail.com>
2019-07-23 13:58:16 +0200

    first commit

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+from app import app
+####  Notes ####
+# type 'python' in windows terminal to check that it is working and to activate it
+# to create virtual environment directory in your app, be in the right dir and in shell type 'python -m venv venv'
+# to activate the virtual environment so that you can use it - 'venv\scripts\activate'
+# if you still need to install flask then now 'pip install flask' otherwise skip this step if it is already installed
+# to confirm that your virtual environment has pip installed - start the python interpreter and 'import flask'
+#  *** to run app***
+# import flask '(venv) $ export FLASK_APP=microblog.py(nameofyourapplication.py)'
+# (venv) $ flask run
