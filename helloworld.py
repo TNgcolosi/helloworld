dc470802f2d5e20115cb3f26a7e642cfa46e76c7
Thandeka Bhengu <mazibani@gmail.com>
2019-09-11 22:53:35 +0200

    .gitignore fix

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,10 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
 ####  Notes ####
 # type 'python' in windows terminal to check that it is working and to activate it
 # to create virtual environment directory in your app, be in the right dir and in shell type 'python -m venv venv'
@@ -6,5 +12,5 @@ from app import app
 # if you still need to install flask then now 'pip install flask' otherwise skip this step if it is already installed
 # to confirm that your virtual environment has pip installed - start the python interpreter and 'import flask'
 #  *** to run app***
-# import flask '(venv) $ export FLASK_APP=microblog.py(nameofyourapplication.py)'
+# import flask '(venv) $ set FLASK_APP=microblog.py(nameofyourapplication.py)'
 # (venv) $ flask run
